861634db88efc38d72db9dcd2bd29575c09ac858
Carlos Aguilera <ckarlos_95@hotmail.es>
2022-10-11 20:20:30 -0500

    Add DialogFlow controller

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import App from '@/app';
 import { IndexController } from '@controllers/index.controller';
+import { DialogFlowController } from '@controllers/dialog-flow.controller';
 import validateEnv from '@utils/validateEnv';
 
 validateEnv();
 
-const app = new App([IndexController]);
+const app = new App([IndexController, DialogFlowController]);
 app.listen();
